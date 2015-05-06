791a6fc60ac83e86aa53ab4b510bc59d7e958fb8
Florian Schulze <florian.schulze@gmx.net>
2015-05-06 16:12:03 +0200

    Test POST data.

--- tests/test_jenkins.py
+++ tests/test_jenkins.py
@@ -21,11 +21,25 @@ def test_create_index_with_jenkinsurl(mapp):
 
 @pytest.mark.notransaction
 def test_upload_with_jenkins(mapp, reqmock):
+    from io import BytesIO
+    import cgi
+    import json
     mapp.create_and_use()
     mapp.set_indexconfig_option("uploadtrigger_jenkins", "http://x.com/{pkgname}/{pkgversion}")
     rec = reqmock.mockresponse(code=200, url=None)
     mapp.upload_file_pypi("pkg1-2.6.tgz", b"123", "pkg1", "2.6", code=200)
     assert len(rec.requests) == 1
-    assert rec.requests[0].url == "http://x.com/pkg1/2.6"
-    # XXX properly decode form
-    # assert args[1]["data"]["Submit"] == "Build"
+    req = rec.requests[0]
+    assert req.url == "http://x.com/pkg1/2.6"
+    fs = cgi.FieldStorage(
+        BytesIO(req.body), req.headers, environ=dict(REQUEST_METHOD='POST'))
+    assert fs.getfirst("Submit") == "Build"
+    assert json.loads(fs.getfirst("json")) == {
+        "parameter": {"file": "file0", "name": "jobscript.py"}}
+    assert fs.getfirst("name") == "jobscript.py"
+    script = fs.getfirst("file0")
+    assert script.startswith(b'#!/')
+    assert b'indexurl = "http://localhost/user1/dev"' in script
+    assert b'virtualenvtar_url = "http://localhost/root/pypi/+f/f61/cdd983d2c4e6a/virtualenv-1.11.6.tar.gz"' in script
+    assert b'devpi_install_index = "http://localhost/user1/dev/+simple/"' in script
+    assert b'testspec = "pkg1"' in script
