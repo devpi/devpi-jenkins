a77b7d1552d0b15baf19f0ab635b8acb00293874
Florian Schulze <florian.schulze@gmx.net>
2015-11-24 17:26:17 +0100

    Fixes for devpi-server 3.0.0.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from test_devpi_server.conftest import gentmp, httpget, makemapp  # noqa
 from test_devpi_server.conftest import maketestapp, makexom, mapp  # noqa
 from test_devpi_server.conftest import pypiurls, testapp  # noqa
-from test_devpi_server.conftest import mock, proxymock, reqmock  # noqa
+from test_devpi_server.conftest import storage_info  # noqa
+from test_devpi_server.conftest import mock, reqmock  # noqa
